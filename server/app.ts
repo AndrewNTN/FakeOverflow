1c2d1ac0391515cddbabf8b96b197924f1548c57
AndrewNTN <andrewton80@gmail.com>
2024-07-07 23:52:24 -0400

    Update app.ts

--- server/app.ts
+++ server/app.ts
@@ -30,10 +30,10 @@ app.use(
   }),
 );
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "client/dist", "index.html"));
 });
 
 const mongoURI = process.env.MONGO_URI || "";
